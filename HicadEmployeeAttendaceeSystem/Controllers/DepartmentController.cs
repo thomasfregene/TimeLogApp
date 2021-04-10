667369691e52cb0544f5e747eab37bf43d2e2b89
thomasfregene <fregzythomas@gmail.com>
2021-04-10 04:07:59 +0100

    Added Department Controller

--- HicadEmployeeAttendaceeSystem/Controllers/DepartmentController.cs
+++ HicadEmployeeAttendaceeSystem/Controllers/DepartmentController.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using HicadEmployeeAttendaceeSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HicadEmployeeAttendaceeSystem.Controllers
 {
-    public class DepartmentController
+    [Route("api/[controller]")] // api/department
+    [ApiController()]
+    public class DepartmentController : ControllerBase
     {
+        private readonly ApiDbContext _context;
+
+        public DepartmentController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDepartments()
+        {
+            var department = await _context.Departments.ToListAsync();
+            return Ok(department);
+        }
     }
 }
