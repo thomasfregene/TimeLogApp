da0202f9af60091dfa3a6cd7d685be43d8d6ac60
Fregene Thomas <fregzythomas@gmail.com>
2021-04-16 15:55:32 +0100

    reference store in app.js file

--- HicadEmployeeAttendanceSystem.UI/vue/app.js
+++ HicadEmployeeAttendanceSystem.UI/vue/app.js
@@ -1,8 +1,12 @@
-const Vue = require('vue');
+window.axios=require('axios')
+window.Vue=require('vue')
+
+import store from './store'
 
 const files = require.context('./', true, /\.vue$/i)
 
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 const app = new Vue({
-    el: "#app"
+    el: "#app",
+    store
 });
