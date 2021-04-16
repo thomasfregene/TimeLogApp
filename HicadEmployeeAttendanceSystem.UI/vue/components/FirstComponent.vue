330c6b9c4a9724d3dc9f8c922d1cf39138bdfd52
Fregene Thomas <fregzythomas@gmail.com>
2021-04-16 13:24:51 +0100

    added configuration for laravel-mix

---
+++ HicadEmployeeAttendanceSystem.UI/vue/components/FirstComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        The year is {{ year }}
+    </div>
+</template>
+<script>
+    module.exports = {
+        data() {
+            return {
+                year: new Date().getFullYear()
+            }
+        }
+    }
+</script>
